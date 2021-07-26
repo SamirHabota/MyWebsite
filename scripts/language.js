464e450c9d2d9a22412e412a690e51878807a492
Samir Habota <samirha@ad-pro.info>
2021-07-26 16:25:04 +0200

    contentless website template

--- scripts/language.js
+++ scripts/language.js
@@ -2,18 +2,72 @@ function loadEnglish() {
   document.getElementById("lang-startButton").innerHTML = "let's go";
   document.getElementById("lang-mainTitle").innerHTML = "learn";
   document.getElementById("lang-subTitle").innerHTML = "then do IT all";
+
+  document.getElementById("lang-aboutLink").innerHTML = "about";
+  document.getElementById("lang-coursesLink").innerHTML = "courses";
+  document.getElementById("lang-teachingLink").innerHTML = "teaching";
+  document.getElementById("lang-pricingLink").innerHTML = "pricing";
+  document.getElementById("lang-reviewsLink").innerHTML = "reviews";
+  document.getElementById("lang-contactLink").innerHTML = "contact";
+
+  document.getElementById("lang-aboutLinkMobile").innerHTML = "about";
+  document.getElementById("lang-coursesLinkMobile").innerHTML = "courses";
+  document.getElementById("lang-teachingLinkMobile").innerHTML = "teaching";
+  document.getElementById("lang-pricingLinkMobile").innerHTML = "pricing";
+  document.getElementById("lang-reviewsLinkMobile").innerHTML = "reviews";
+  document.getElementById("lang-contactLinkMobile").innerHTML = "contact";
+
+  document.getElementById("lang-backToTop").innerHTML = "back to top";
+
+  document.getElementById("lang-websiteTitle").innerHTML = "MyWebsite";
 }
 
 function loadBosnian() {
   document.getElementById("lang-startButton").innerHTML = "pokreni";
   document.getElementById("lang-mainTitle").innerHTML = "nauči";
-  document.getElementById("lang-subTitle").innerHTML = "onda radi sve";
+  document.getElementById("lang-subTitle").innerHTML = "da radiš sve";
+
+  document.getElementById("lang-aboutLink").innerHTML = "o meni";
+  document.getElementById("lang-coursesLink").innerHTML = "kursevi";
+  document.getElementById("lang-teachingLink").innerHTML = "podučavanje";
+  document.getElementById("lang-pricingLink").innerHTML = "cjenovnik";
+  document.getElementById("lang-reviewsLink").innerHTML = "utisci";
+  document.getElementById("lang-contactLink").innerHTML = "kontakt";
+
+  document.getElementById("lang-aboutLinkMobile").innerHTML = "o meni";
+  document.getElementById("lang-coursesLinkMobile").innerHTML = "kursevi";
+  document.getElementById("lang-teachingLinkMobile").innerHTML = "podučavanje";
+  document.getElementById("lang-pricingLinkMobile").innerHTML = "cjenovnik";
+  document.getElementById("lang-reviewsLinkMobile").innerHTML = "utisci";
+  document.getElementById("lang-contactLinkMobile").innerHTML = "kontakt";
+
+  document.getElementById("lang-backToTop").innerHTML = "nazad na vrh";
+
+  document.getElementById("lang-websiteTitle").innerHTML = "MojaStranica";
 }
 
 function loadGerman() {
   document.getElementById("lang-startButton").innerHTML = "los gehts";
   document.getElementById("lang-mainTitle").innerHTML = "lerne";
   document.getElementById("lang-subTitle").innerHTML = "dann mach alles";
+
+  document.getElementById("lang-aboutLink").innerHTML = "über";
+  document.getElementById("lang-coursesLink").innerHTML = "kurse";
+  document.getElementById("lang-teachingLink").innerHTML = "unterricht";
+  document.getElementById("lang-pricingLink").innerHTML = "preisliste";
+  document.getElementById("lang-reviewsLink").innerHTML = "bewertungen";
+  document.getElementById("lang-contactLink").innerHTML = "kontakt";
+
+  document.getElementById("lang-aboutLinkMobile").innerHTML = "über";
+  document.getElementById("lang-coursesLinkMobile").innerHTML = "kurse";
+  document.getElementById("lang-teachingLinkMobile").innerHTML = "unterricht";
+  document.getElementById("lang-pricingLinkMobile").innerHTML = "preisliste";
+  document.getElementById("lang-reviewsLinkMobile").innerHTML = "bewertungen";
+  document.getElementById("lang-contactLinkMobile").innerHTML = "kontakt";
+
+  document.getElementById("lang-backToTop").innerHTML = "zurück nach oben";
+
+  document.getElementById("lang-websiteTitle").innerHTML = "MeineSeite";
 }
 
 function onToggleClick() {
